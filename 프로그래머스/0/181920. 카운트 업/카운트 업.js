ec9d836e967b4ad2315648369672e8bf41bec2c6
김민아 <kmina121777@naver.com>
2026-05-24 18:05:00 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,10 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    const i = 0;
+    
+    while(start_num<=end_num){
+        answer.push(start_num);
+        start_num++;
+    }
+    return answer;
+}
